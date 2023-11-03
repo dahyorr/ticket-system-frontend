0eea063ff0de6c6bbd09b978a9668a33b3d1dc2b
Dayo Adebanjo <dahyor@outlook.com>
2023-11-03 18:16:10 +0100

    update ui

--- src/Components/auth/Login.js
+++ src/Components/auth/Login.js
@@ -1,37 +1,52 @@
-import React from 'react'
+import React from 'react';
 import 'react-toastify/dist/ReactToastify.css';
-import { toast } from 'react-toastify'
-import LoginForm from "../forms/LoginForm";
-import {useAuth} from '../../hooks'
+import { toast } from 'react-toastify';
+import LoginForm from '../forms/LoginForm';
+import { useAuth } from '../../hooks';
 
-const Login = ({history,location}) => {
+const Login = ({ history, location }) => {
     const queryString = location.search;
     const sp = new URLSearchParams(queryString);
-    const next = sp.get('next')
-    const {logIn} = useAuth() 
+    const next = sp.get('next');
+    const { logIn } = useAuth();
 
-    const onLogin = async ({email, password}) => {
+    const onLogin = async ({ email, password }) => {
         // TODO: showLoader
-        const res = await logIn(email, password)
-        if (res.status === 'error'){
-            toast.error(res.message)
+        const res = await logIn(email, password);
+        if (res.status === 'error') {
+            toast.error(res.message);
+        } else {
+            history.push(next || '/');
         }
-        else{
-            history.push(next || '/')
-        }
-    }
+    };
 
     return (
-            <div className={'Login'}>
-                <div className="content">
-                    <div className="logo"><h1>Ticketrr</h1></div>
-                    <h2>Sign in to your account</h2>
-                    <div className="form-container">
-                        <LoginForm onFormSubmit={onLogin}/>
-                    </div>
+        <div className={'Login'}>
+            <div className="content">
+                <div className="logo">
+                    <h1>Ticketrr</h1>
+                </div>
+                <h2>Sign in to your account</h2>
+                <div className="form-container">
+                    <LoginForm onFormSubmit={onLogin} />
                 </div>
             </div>
-    )
-}
+            <div className="info">
+                <p>Use the credentials below to sign in</p>
 
-export default (Login)
+                <b>
+                    <p> email: test@test.com</p>
+                </b>
+                <b>
+                    <p> password: tryingoutdemo</p>
+                </b>
+                <p>
+                    PS: It might take a while to spin up the backend, its free
+                    hosting
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- src/Components/auth/Register.js
+++ src/Components/auth/Register.js
@@ -1,38 +1,51 @@
-import React from 'react'
-import SignupForm from "../forms/SignupForm";
+import React from 'react';
+import SignupForm from '../forms/SignupForm';
 import 'react-toastify/dist/ReactToastify.css';
-import { toast } from 'react-toastify'
-import {useAuth} from '../../hooks'
+import { toast } from 'react-toastify';
+import { useAuth } from '../../hooks';
 
-const Register = ({history})=>{
-    const {signUp} = useAuth() 
+const Register = ({ history }) => {
+    const { signUp } = useAuth();
 
-    const onSignup = async ({name, email, password}) => {
+    const onSignup = async ({ name, email, password }) => {
         // TODO: showLoader
-        const res = await signUp(name, email, password)
-        console.log(res)
-        if (res.status === 'error'){
-            toast.error(res.message)
+        const res = await signUp(name, email, password);
+        console.log(res);
+        if (res.status === 'error') {
+            toast.error(res.message);
+        } else {
+            if (res.message) toast.success(res.message);
+            history.push('/');
         }
-        else{
-            if(res.message) toast.success(res.message)
-            history.push('/')
-        }
-    }
-
+    };
 
     return (
         <div className={'Register'}>
             <div className="content">
-                <div className="logo"><h1>Ticketrr</h1></div>
+                <div className="logo">
+                    <h1>Ticketrr</h1>
+                </div>
                 <h2>Create a new account</h2>
                 <div className="form-container">
-                    <SignupForm onFormSubmit={onSignup}/>
+                    <SignupForm onFormSubmit={onSignup} />
                 </div>
             </div>
-            
-        </div>
-    )
-}
+            <div className="info">
+                <p>Use the credentials below to sign in</p>
 
-export default Register
+                <b>
+                    <p> email: test@test.com</p>
+                </b>
+                <b>
+                    <p> password: tryingoutdemo</p>
+                </b>
+                <p>
+                    PS: It might take a while to spin up the backend, its free
+                    hosting
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
