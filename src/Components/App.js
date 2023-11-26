deccd4697e1cd7623e445269ab04343925c9a484
Dayo Adebanjo <dahyor@outlook.com>
2023-11-26 14:36:27 +0100

    add vercel analytics

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import {Router, Switch, Route} from "react-router-dom";
-import {PrivateRoute, PublicRoute} from "./auth/RouteTypes";
-import { ToastContainer } from "react-toastify";
+import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import { PrivateRoute, PublicRoute } from './auth/RouteTypes';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './auth/Login';
 import Register from './auth/Register';
@@ -10,43 +10,73 @@ import Tickets from './views/Tickets';
 import TicketView from './views/TicketView';
 import NewTicket from './views/NewTicket';
 import CreatedTickets from './views/CreatedTickets';
-import NotFound from "./error/NotFound";
-import Layout from './common/Layout'
-import { AuthProvider } from "../context/AuthContext";
-import { TicketProvider } from "../context/TicketContext";
+import NotFound from './error/NotFound';
+import Layout from './common/Layout';
+import { AuthProvider } from '../context/AuthContext';
+import { TicketProvider } from '../context/TicketContext';
 import OpenTickets from './views/OpenTickets';
 import UsersList from './views/UsersList';
-import {history} from '../utils'
+import { history } from '../utils';
+import { Analytics } from '@vercel/analytics/react';
 
-const App = () =>{
+const App = () => {
     return (
         <div className="App">
             <AuthProvider>
-            <TicketProvider>
-                <ToastContainer/>
-                <Router history={history}>
-                    <Switch>
-                        <PublicRoute path={'/login'}  component={Login}/>
-                        <PublicRoute path={'/register'}  component={Register}/>
-                        <PrivateRoute path={'/'} exact component={Dashboard}/>
-                        <PrivateRoute path={'/tickets'} exact component={Tickets}/>
-                        <PrivateRoute path={'/tickets/create'}  component={NewTicket}/>
-                        <PrivateRoute path={'/open-tickets'}  component={OpenTickets}/>
-                        <PrivateRoute path={'/tickets/:id'} component={TicketView}/> 
-                        <PrivateRoute path={'/user/tickets'} component={CreatedTickets}/> 
-                        <PrivateRoute path={'/users'} component={UsersList}/> 
+                <TicketProvider>
+                    <ToastContainer />
+                    <Router history={history}>
+                        <Switch>
+                            <PublicRoute path={'/login'} component={Login} />
+                            <PublicRoute
+                                path={'/register'}
+                                component={Register}
+                            />
+                            <PrivateRoute
+                                path={'/'}
+                                exact
+                                component={Dashboard}
+                            />
+                            <PrivateRoute
+                                path={'/tickets'}
+                                exact
+                                component={Tickets}
+                            />
+                            <PrivateRoute
+                                path={'/tickets/create'}
+                                component={NewTicket}
+                            />
+                            <PrivateRoute
+                                path={'/open-tickets'}
+                                component={OpenTickets}
+                            />
+                            <PrivateRoute
+                                path={'/tickets/:id'}
+                                component={TicketView}
+                            />
+                            <PrivateRoute
+                                path={'/user/tickets'}
+                                component={CreatedTickets}
+                            />
+                            <PrivateRoute
+                                path={'/users'}
+                                component={UsersList}
+                            />
 
-                        <Route render={(props) => (
-                            <Layout>
-                                <NotFound {...props}/>
-                            </Layout>
-                        )}/>
-                    </Switch>
-                </Router>
-            </TicketProvider>
+                            <Route
+                                render={(props) => (
+                                    <Layout>
+                                        <NotFound {...props} />
+                                    </Layout>
+                                )}
+                            />
+                        </Switch>
+                    </Router>
+                </TicketProvider>
             </AuthProvider>
+            <Analytics />
         </div>
     );
-}
+};
 
 export default App;
